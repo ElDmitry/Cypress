bf1173b9a7c857ea103da3305c33644ce7452575
Dmytro Bilozertsev <dp290691bdo@gmail.com>
2025-02-09 16:36:14 +0200

    add hw

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
   videosFolder: "cypress/videos",
   video: true,
   modifyObstructiveCode: false,
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
